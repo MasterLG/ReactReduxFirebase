921b22c2d53893fa249a2f1203e854c8f921768a
Levent GUZELOGLU <gun.guzeloglu@gmail.com>
2018-09-21 15:10:22 +0300

    added parts 7 and 8

---
+++ marioplan/src/components/projects/ProjectDetails.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const ProjectDetails = props => {
+  const id = props.match.params.id;
+  //props.history.replaceState({});
+  return (
+    <div className="container section project-details">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Project Title - {id}</span>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
+        <div className="card-action grey lighten-4 grey-text">
+          <div>Posted by The Net Ninja</div>
+          <div>2nd September, 2am</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectDetails;
