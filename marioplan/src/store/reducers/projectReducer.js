a925b2dcc16e4ba82b6a857ec18cca7942932fa5
Levent GUZELOGLU <gun.guzeloglu@gmail.com>
2018-10-03 17:15:44 +0300

    End of Part 17 and 18. Create project works.

--- marioplan/src/store/reducers/projectReducer.js
+++ marioplan/src/store/reducers/projectReducer.js
@@ -9,11 +9,13 @@ const projectReducer = (state = initialState, action) => {
   switch (action.type) {
     case "CREATE_PROJECT":
       console.log("created_project", action.project);
-
+      return state;
+    case "CREATE_PROJECT_ERROR":
+      console.log("create project error", action.err);
+      return state;
     default:
-      break;
+      return state;
   }
-  return state;
 };
 
 export default projectReducer;
